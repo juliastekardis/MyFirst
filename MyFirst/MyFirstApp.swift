6f48bb41236f843ee44af5a722ab1558063af368
juliastekardis <juliastekardis@gmail.com>
2023-09-01 14:14:18 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Julia Stekardis on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
